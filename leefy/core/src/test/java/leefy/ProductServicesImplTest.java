6f8cdb77d9d8c1a883b7a0be69ecad00d5bba15b
Jeewantha Samaraweera <jeewantha25@gmail.com>
2014-11-09 06:40:32 +0530

    Updated with unit testing. Rest assuered is used for unit testing.

--- leefy/core/src/test/java/leefy/ProductServicesImplTest.java
+++ leefy/core/src/test/java/leefy/ProductServicesImplTest.java
@@ -1,14 +1,27 @@
 package leefy;
 
-import static org.junit.Assert.fail;
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:**/resources/beans-spring.xml",
+		"classpath:**/resources/rest-service-beans.xml", "classpath:**/resources/security-context.xml" })
 public class ProductServicesImplTest {
 
 	@Test
-	public void testGetProduct() {
-		fail("Not yet implemented");
+	public void testGetProduct() throws Exception {
+
+
+		get("/leefy/services/productServices/product/123").then().body(
+				"productId", equalTo("123"));
 	}
 
 }
